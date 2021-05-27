4bd3ef6fcde32edfd938eb6d5bf9e12b69fe3fc7
Elranoren <elranoren@gmail.com>
2021-05-27 13:39:03 +0300

    Start3

---
+++ src/Model/IModel.java
@@ -0,0 +1,6 @@
+package Model;
+
+public interface IModel {
+    void generateMaze(int row , int column);
+    void solveMaze();
+}
---
+++ src/Model/MyModel.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class MyModel implements IModel{
+    @Override
+    public void generateMaze(int row, int column) {
+
+    }
+
+    @Override
+    public void solveMaze() {
+
+    }
+}
