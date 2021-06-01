85d56e3fabde06323011ffadc3b466a9c883f0b6
TamarTzuberi <tamar.tzuberi1@gmail.com>
2021-06-01 16:15:31 +0300

    SettingsScene.fxml

--- src/Model/IModel.java
+++ src/Model/IModel.java
@@ -1,6 +1,19 @@
 package Model;
 
+import Server.Configurations;
+import algorithms.mazeGenerators.IMazeGenerator;
+
 public interface IModel {
     void generateMaze(int row , int column);
     void solveMaze();
+
+//    static int getThreadsNumConfig(){
+//       // return Configurations.p.getProperty("mazeGeneratingAlgorithm");
+//    }
+//
+//    static IMazeGenerator getGeneratingAlgorithmConfig(){
+//    }
+//
+//    static String getSearchAlgorithmConfig(){
+//    }
 }
